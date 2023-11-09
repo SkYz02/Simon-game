a2f2fdcfcf58dae4116581675f0ebc9fd2dd0f32
Your Name <you@example.com>
2023-11-09 19:54:39 +0530

    ALl set

--- game.js
+++ game.js
@@ -62,18 +62,20 @@ function startOver(){
     started = false;
 }
 
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-
-}
-
 function animatePress(currentColour){
     $("#"+currentColour).addClass("pressed");
 
     setTimeout(function(){
         $("#"+currentColour).removeClass("pressed");},100);
 }
+
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+
+}
+
 
 
 
